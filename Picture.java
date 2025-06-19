2a058687b2c853fd0f7da698715786be27167035
incizors <thatoneguywhoscool@gmail.com>
2025-06-18 21:13:47 -0700

    Create Picture.java
    Added the Picture class.

---
+++ Picture.java
@@ -0,0 +1,20 @@
+public class Picture {
+    private String path;
+
+    public Picture(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public String toString() {
+        return path;
+    }
+}
